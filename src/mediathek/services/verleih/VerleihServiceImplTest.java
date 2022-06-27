e0ce3a499bd170c3fe6cc515326fc709b2a22515
Shamveel Ashfaq <108290181+K0deC0de@users.noreply.github.com>
2022-06-27 20:04:41 +0200

    Add files via upload

---
+++ src/mediathek/services/verleih/VerleihServiceImplTest.java
@@ -0,0 +1,211 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.services.verleih;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Datum;
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Kundennummer;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.Kunde;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.Verleihkarte;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.CD;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.ServiceObserver;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.kundenstamm.KundenstammService;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.kundenstamm.KundenstammServiceImpl;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.medienbestand.MedienbestandService;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.medienbestand.MedienbestandServiceImpl;
+
+/**
+ * @author SE2-Team
+ */
+public class VerleihServiceImplTest
+{
+    private Datum _datum;
+    private Kunde _kunde;
+    private VerleihService _service;
+    private List<Medium> _medienListe;
+    private Kunde _vormerkkunde1;
+    private Kunde _vormerkkunde2;
+    private Kunde _vormerkkunde3;
+	private Kunde _vormerkkunde4;
+
+    public VerleihServiceImplTest()
+    {
+        _datum = new Datum(3, 4, 2009);
+        KundenstammService kundenstamm = new KundenstammServiceImpl(
+                new ArrayList<Kunde>());
+        _kunde = new Kunde(new Kundennummer(123456), "ich", "du");
+
+        _vormerkkunde1 = new Kunde(new Kundennummer(666999), "peter", "dingens");
+        _vormerkkunde2 = new Kunde(new Kundennummer(767676), "lars", "müller");
+        _vormerkkunde3 = new Kunde(new Kundennummer(666777), "paul", "panter");
+        _vormerkkunde4 = new Kunde(new Kundennummer(666997), "mieser", "kadser");
+
+        kundenstamm.fuegeKundenEin(_kunde);
+        kundenstamm.fuegeKundenEin(_vormerkkunde1);
+        kundenstamm.fuegeKundenEin(_vormerkkunde2);
+        kundenstamm.fuegeKundenEin(_vormerkkunde3);
+        kundenstamm.fuegeKundenEin(_vormerkkunde4);
+                
+        MedienbestandService medienbestand = new MedienbestandServiceImpl(
+                new ArrayList<Medium>());
+        Medium medium = new CD("CD1", "baz", "foo", 123);
+        medienbestand.fuegeMediumEin(medium);
+        medium = new CD("CD2", "baz", "foo", 123);
+        medienbestand.fuegeMediumEin(medium);
+        medium = new CD("CD3", "baz", "foo", 123);
+        medienbestand.fuegeMediumEin(medium);
+        medium = new CD("CD4", "baz", "foo", 123);
+        medienbestand.fuegeMediumEin(medium);
+        _medienListe = medienbestand.getMedien();
+        _service = new VerleihServiceImpl(kundenstamm, medienbestand,
+                new ArrayList<Verleihkarte>());
+    }
+
+    @Test
+    public void testeNachInitialisierungIstNichtsVerliehen() throws Exception
+    {
+        assertTrue(_service.getVerleihkarten()
+            .isEmpty());
+        assertFalse(_service.istVerliehen(_medienListe.get(0)));
+        assertFalse(_service.sindAlleVerliehen(_medienListe));
+        assertTrue(_service.sindAlleNichtVerliehen(_medienListe));
+    }
+
+    @Test
+    public void testeVerleihUndRueckgabeVonMedien() throws Exception
+    {
+        // Lege eine Liste mit nur verliehenen und eine Liste mit ausschließlich
+        // nicht verliehenen Medien an
+        List<Medium> verlieheneMedien = _medienListe.subList(0, 2);
+        List<Medium> nichtVerlieheneMedien = _medienListe.subList(2, 4);
+        _service.verleiheAn(_kunde, verlieheneMedien, _datum);
+
+        // Prüfe, ob alle sondierenden Operationen für das Vertragsmodell
+        // funktionieren
+        assertTrue(_service.istVerliehen(verlieheneMedien.get(0)));
+        assertTrue(_service.istVerliehen(verlieheneMedien.get(1)));
+        assertFalse(_service.istVerliehen(nichtVerlieheneMedien.get(0)));
+        assertFalse(_service.istVerliehen(nichtVerlieheneMedien.get(1)));
+        assertTrue(_service.sindAlleVerliehen(verlieheneMedien));
+        assertTrue(_service.sindAlleNichtVerliehen(nichtVerlieheneMedien));
+        assertFalse(_service.sindAlleNichtVerliehen(verlieheneMedien));
+        assertFalse(_service.sindAlleVerliehen(nichtVerlieheneMedien));
+        assertFalse(_service.sindAlleVerliehen(_medienListe));
+        assertFalse(_service.sindAlleNichtVerliehen(_medienListe));
+        assertTrue(_service.istVerliehenAn(_kunde, verlieheneMedien.get(0)));
+        assertTrue(_service.istVerliehenAn(_kunde, verlieheneMedien.get(1)));
+        assertFalse(
+                _service.istVerliehenAn(_kunde, nichtVerlieheneMedien.get(0)));
+        assertFalse(
+                _service.istVerliehenAn(_kunde, nichtVerlieheneMedien.get(1)));
+        assertTrue(_service.sindAlleVerliehenAn(_kunde, verlieheneMedien));
+        assertFalse(
+                _service.sindAlleVerliehenAn(_kunde, nichtVerlieheneMedien));
+
+        // Prüfe alle sonstigen sondierenden Methoden
+        assertEquals(2, _service.getVerleihkarten()
+            .size());
+
+        _service.nimmZurueck(verlieheneMedien, _datum);
+        // Prüfe, ob alle sondierenden Operationen für das Vertragsmodell
+        // funktionieren
+        assertFalse(_service.istVerliehen(verlieheneMedien.get(0)));
+        assertFalse(_service.istVerliehen(verlieheneMedien.get(1)));
+        assertFalse(_service.istVerliehen(nichtVerlieheneMedien.get(0)));
+        assertFalse(_service.istVerliehen(nichtVerlieheneMedien.get(1)));
+        assertFalse(_service.sindAlleVerliehen(verlieheneMedien));
+        assertTrue(_service.sindAlleNichtVerliehen(nichtVerlieheneMedien));
+        assertTrue(_service.sindAlleNichtVerliehen(verlieheneMedien));
+        assertFalse(_service.sindAlleVerliehen(nichtVerlieheneMedien));
+        assertFalse(_service.sindAlleVerliehen(_medienListe));
+        assertTrue(_service.sindAlleNichtVerliehen(_medienListe));
+        assertTrue(_service.getVerleihkarten()
+            .isEmpty());
+    }
+
+    @Test
+    public void testVerleihEreignisBeobachter() throws ProtokollierException
+    {
+        final boolean ereignisse[] = new boolean[1];
+        ereignisse[0] = false;
+        ServiceObserver beobachter = new ServiceObserver()
+        {
+            @Override
+            public void reagiereAufAenderung()
+            {
+                ereignisse[0] = true;
+            }
+        };
+        _service.verleiheAn(_kunde,
+                Collections.singletonList(_medienListe.get(0)), _datum);
+        assertFalse(ereignisse[0]);
+
+        _service.registriereBeobachter(beobachter);
+        _service.verleiheAn(_kunde,
+                Collections.singletonList(_medienListe.get(1)), _datum);
+        assertTrue(ereignisse[0]);
+
+        _service.entferneBeobachter(beobachter);
+        ereignisse[0] = false;
+        _service.verleiheAn(_kunde,
+                Collections.singletonList(_medienListe.get(2)), _datum);
+        assertFalse(ereignisse[0]);
+    }
+    
+    @Test
+    public void testeVormerkenUndAusleihenUndNachrücken() throws ProtokollierException
+    {
+        _service.merkeVor(_vormerkkunde1, _medienListe);
+		_service.merkeVor(_vormerkkunde2, _medienListe);
+		_service.merkeVor(_vormerkkunde3, _medienListe);
+		
+		// Schaut, ob jeder hinzugefügte Kunde an der richtigen Stelle vorgemerkt ist.
+		for(Medium medium : _medienListe)
+		{
+			assertEquals(_vormerkkunde1, _service.gibKundeByIndex(0, medium));
+			assertEquals(_vormerkkunde2, _service.gibKundeByIndex(1, medium));
+			assertEquals(_vormerkkunde3, _service.gibKundeByIndex(2, medium));
+		}
+		_service.verleiheAn(_vormerkkunde1, _medienListe, _datum);
+		
+		// Schaut, ob Kunde1 Ausleihen konnte und ob der Rest nachgerückt ist.
+		for(Medium medium : _medienListe)
+		{
+			assertFalse(_service.gibVormerkkarte(medium).istDrin(_vormerkkunde1));
+			assertEquals(_vormerkkunde2, _service.gibKundeByIndex(0, medium));
+			assertEquals(_vormerkkunde3, _service.gibKundeByIndex(1, medium));
+			assertNull(_service.gibKundeByIndex(2, medium));
+		}
+    }
+    
+    @Test
+    public void testeIstVormerkenMoeglich()
+    {
+        _service.merkeVor(_vormerkkunde1, _medienListe);
+		_service.merkeVor(_vormerkkunde2, _medienListe);
+		
+		// Kunde1 ist bereits in der Liste und sollte nicht mehr hinzugefügt werden können.
+		// Kunde3 ist noch nicht in der Liste und hinzufügen sollte möglich sein.
+		assertFalse(_service.istVomerkungMoeglich(_vormerkkunde1, _medienListe));
+		assertTrue(_service.istVomerkungMoeglich(_vormerkkunde3, _medienListe));
+
+		_service.merkeVor(_vormerkkunde3, _medienListe);
+		
+		// Die Liste hat bereits 3 Vormerker und sollte somit keinen 4. Kunden aufnehmen können.
+		assertFalse(_service.istVomerkungMoeglich(_vormerkkunde4, _medienListe));
+		
+    }
+
+
+
+
+}
